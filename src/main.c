95058302616f0c71019099676876f9018cc37b27
Aissa Laribi <aoifemullen@aoifes-air.home>
2024-03-04 12:10:20 +0000

    Printing first line months pointer strings

--- src/main.c
+++ src/main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#include <math.h>
 
 
 
-enum month {january,february,march,april,may,june,july,august,september,october,november,december};
+
 
 struct task
 {
@@ -14,11 +16,59 @@ struct task
     int numDependencies;
     int dependentTasks[9];
 };
+/* Structs to define points on x-axis and y-axis*/
+struct point{
+	int x;
+	int y;
+};
+/*This struct define two points to be joined*/
+struct line{
+	struct point p1;
+	struct point p2;
+};
 
 void functionEdit(struct task tasks[], int nTasks);
-void test (struct task tasks[]);
-void quit (struct task tasks[]);
+bool functionTest (struct task tasks[], int nTasks);
+bool hasCircularDependency(struct task tasks[], int nTasks, int taskIndex, bool visited[]);
 void printFunction(struct task tasks[], int ntasks);
+
+bool functionTest (struct task tasks[], int nTasks){
+    bool visited[nTasks]; //array to track which tasks have been checked
+    // initialise all elements in array to false as none have been visited
+    for (int i = 0; i < nTasks; ++i) {
+        visited[i] = false;
+    }
+
+    for (int i = 0; i < nTasks; ++i) {
+        //if the ith task hasnt been visited/checked
+        if (!visited[i]) {
+            //we then use another function to check if this task has a circular dependancy
+            if (hasCircularDependency(tasks, nTasks, i, visited)) {
+                //it takes in i as the task index and visited, the array to tell us which task has been visited
+                return false; //return false if circular dependency found
+            }
+        }
+    }
+    return true; // true if no circular dependancy found
+}
+
+bool hasCircularDependency(struct task tasks[], int nTasks, int taskIndex, bool visited[]){
+    visited[taskIndex] = true;
+    for (int i = 0; i < tasks[taskIndex].numDependencies; ++i) {
+        int dependentTask = tasks[taskIndex].dependentTasks[i];
+        if (!visited[dependentTask]) {
+            if (hasCircularDependency(tasks, nTasks, dependentTask, visited)) {
+                return true;
+            }
+        } else if (visited[dependentTask]) {
+            return true; // Circular dependency found
+        }
+    }
+    visited[taskIndex] = false;
+    return false;
+}
+
+
 
 void functionEdit(struct task tasks[], int nTasks){
     char change[20];
@@ -104,10 +154,10 @@ int main(void){
     /*Create a blank task*/
     struct task blank = { " ", 0 , 0, 0, 0};
 
-    /*Initialise all the tasks with blank*/
+    /*Initialize all the tasks with blank*/
     for(int i = 0; i < nTasks; i++){
-    	tasks[i] = blank;
-        }
+        tasks[i] = blank;
+    }
 
     for(int i = 0; i < nTasks; i++){
         /*To enforce the user of not entering more than 20 characters and avoid a security flaw
@@ -162,12 +212,72 @@ int main(void){
             }
         }
     }
+    int userChoice;
+    printf("enter 1 to edit the Gantt, 2 to test if your gant is possible, 3 to quit:\n");
+    scanf("%d", &userChoice);
+    switch (userChoice) {
+        case 1:
+            functionEdit(tasks, nTasks);
+            break;
+        case 2:
+            if(functionTest(tasks, nTasks)){
+                printf("no circular dependencies found\n");
+            }
+            else if(!functionTest(tasks, nTasks)){
+                printf("Gantt not possible due to circular dependency\n");
+            }
+            break;
+        case 3:
+            printf("leaving programme\n");
+            return 0;
+        default:
+            break;
+    }
+
     printFunction(tasks, nTasks);
-    functionEdit(&tasks[nTasks], nTasks);
+
 
 }
 void printFunction(struct task tasks[], int nTasks){
-	for(int i = 0; i < nTasks;i++){
-	    	printf("%-10s %-4d %-4d %-2d \n", tasks[i].taskName, tasks[i].startMonth, tasks[i].endMonth, tasks[i].numDependencies);
-	    }
+		/*Replaced enum with pointer of strings because it is easier for me to iterate through*/
+		const char *month[12] = {"January","February","March","April","May","June","July","August",
+								"September","October","November","December"};
+
+		/*Length of line*/
+		int segment = 200;
+
+		/*Print 200 "_" as a continuous line*/
+		for(int i = 0; i < segment; i++){
+			printf("_");
+		}
+		puts("");
+		/*Leave the first 22 characters free, and then print "|" and the months and the dependencies*/
+		for(int i = 0, j = 0; i < (int) segment; i++){
+			printf(" ");
+			if (i >= 22 && i < 34){
+				printf("| %s", month[j++], i);
+			}
+			else if(i == 35){
+				printf("| %s", "Dependencies");
+			}
+
+		}
+		printf("\n");
+		/*Print the first task so far and trying to repeat the same step as in the previous printed
+		 * line within the months*/
+
+		for(int i = 0; i < (int) segment; i++){
+					printf("-");
+				}
+
+		printf("\n");
+
+		for(int i = 0, j = 0; i < nTasks && j < segment; j++){
+
+			printf("%s%d", tasks[i].taskName, j);
+			j = strlen(tasks[i++].taskName);
+			/*TO COMPLETE: Counting the underscores will help to print the "|"
+			 * I am thinking about a blank struct for that as well
+			 */
+		}
 }
